71b9d6d581a41e07248c0cdf7787349aca96530a
Jacob Bravo <jacobmuthondio@gmail.com>
2024-02-14 22:26:07 +0300

    added redirect links

--- src/Pages/Share/Share.jsx
+++ src/Pages/Share/Share.jsx
@@ -23,13 +23,30 @@ const Share = () => {
       <h2>Share ID TRACKR with a fellow student</h2>
       <div className={css.shareArea}>
         <CircleAvator title={'Copy Link'} />
-        <CircleAvator title={'WhatsApp'} />
-        <CircleAvator title={'Instagram'} />
-        <CircleAvator title={'Telegram'} />
-        <CircleAvator title={'Twitter'} />
-        <CircleAvator title={'Snapchat'} />
-        <CircleAvator title={'Facebook'} />
-        <CircleAvator title={'Email'} />
+        <CircleAvator title={'WhatsApp'} onClick={() => {
+          window.location.href = 'https://api.whatsapp.com/send?phone=0712540930';
+        }} />
+        <CircleAvator title={'Instagram'}
+          onClick={() => {
+            window.location.href = 'https://www.instagram.com/';
+          }}
+        />
+        <CircleAvator title={'Telegram'} onClick={() => {
+          window.location.href = 'https://telegram.org/';
+        }} />
+        <CircleAvator title={'Twitter'} onClick={() => {
+          window.location.href = 'https://www.twitter.com/';
+        }} />
+        <CircleAvator title={'Snapchat'} onClick={() => {
+          window.location.href = 'https://www.snapchat.com/';
+        }} />
+        <CircleAvator title={'Facebook'}
+          onClick={() => {
+            window.location.href = 'https://www.facebook.com/';
+          }} />
+        <CircleAvator title={'Email'} onClick={() => {
+          window.location.href = 'https://www.gmail.com/';
+        }} />
       </div>
     </div>
   )
